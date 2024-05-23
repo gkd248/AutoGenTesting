544bc86e4af4cdca09b5a0a76e1f7b4b2c814411
gkd248 <gdarnell@utexas.edu>
2024-05-23 17:51:14 -0500

    Working example of writing and executing user requested code then terminating

--- basic_code_generation_execution.py
+++ basic_code_generation_execution.py
@@ -15,13 +15,13 @@ llm_config = {"config_list": config_list, "seed": 42, "temperature": 0.0}
 
 engineer_agent_prompt = """You are an engineer able to write python code to solve a problem. 
 The user will give you a problem to solve and you will need to write code to get the answer. 
+Only output runnable code.
 The user will run the code and report any problems or errors, you will then create an updated version of your code to address these as they arise.
-If there are problems please include the full solution including any prior code you write."""
+If the code executes successfully and returns the needed values output TERMINATE."""
 engineer_agent = autogen.AssistantAgent(
     name="engineer_agent_prompt",
     system_message=engineer_agent_prompt,
     llm_config=llm_config,
-    is_termination_msg=lambda x: "SUCCESS" in x.get("content", "").rstrip(),
 )
 
 # to dig a bit more into the code_execution_config, when this agent is prompted it will
@@ -33,23 +33,10 @@ engineer_agent = autogen.AssistantAgent(
 
 executor_user_proxy = autogen.UserProxyAgent(
     name="Executor",
-    system_message="Executor. Execute the code written by the engineer and report the result. "
-                   "Once the code executes successfully with the right output, end the conversation by saying SUCCESS.",
-    human_input_mode="NEVER",
+    system_message="Executor. Execute the code written by the engineer and report the result. ",
     code_execution_config={"last_n_messages": 3, "work_dir": "code_dir", "use_docker": False},
+    human_input_mode="ALWAYS",
     is_termination_msg=lambda x: "TERMINATE" in x.get("content", "").rstrip(),
 )
 
-# our task is pretty straightforward, go to this url and extract some data
-# we also tell the agent we have some common packages installed so it doesn't waste time
-# trying to install them, sometimes the agents will insist on creating bash scripts to install
-# everything without verifying if the environment has the packages first, so it's useful to let
-# the agent know what it does or does not have at their disposal
-initial_message="""Provided url: https://medium.com/@coldstart_coder/basics-of-the-walrus-operator-in-python-a9b18ca1469c
-
-The provided url is for a medium article. I need you to write a script that will fetch the page, parse the html and print out the title of the article, the author and when it was published. 
-
-I have BeautifulSoup and requests installed so I should be good to go there. 
-"""
-# start the conversation!
-executor_user_proxy.initiate_chat(engineer_agent, message=initial_message)
+engineer_agent.initiate_chat(executor_user_proxy, message="Hello! I'm an AI assistant designed to write code. Please tell me what code I should write.")
